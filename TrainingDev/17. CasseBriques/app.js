6380a509534cc39ca5d83941e7ed4d0a466feb26
SaladinX1 <hopefulhorizon01@gmail.com>
2022-04-02 23:35:51 +0200

    Etablissement des fonction collisioDetection brique , ainsi que le mousemouve pour controler la barre, et reload en cas de fin de partie

--- TrainingDev/17. CasseBriques/app.js
+++ TrainingDev/17. CasseBriques/app.js
@@ -1,11 +1,13 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const affichageScore = document.querySelector('.score');
 
 const rayonBalle = 10, barreHeight = 10,  barreWidth = 75,
 nbCol = 8, nbRow = 5, largeurBrique = 75, hauteurBrique = 20;
 
 let x = canvas.width/2, y = canvas.height -30,
-barreX = (canvas.width - barreWidth)/2;
+barreX = (canvas.width - barreWidth)/2, fin = false,
+vitesseX = 5, vitesseY = 5, score = 0;
 
 function dessineBalle() {
 
@@ -16,7 +18,7 @@ function dessineBalle() {
     ctx.closePath();
 }
 
-dessineBalle();
+// dessineBalle();
 
 function dessineBarre() {
 
@@ -28,7 +30,7 @@ function dessineBarre() {
         ctx.closePath();
 }
 
-dessineBarre();
+ // dessineBarre();
 
 // Tableau avec toutes les briques
 
@@ -71,4 +73,113 @@ function dessineBriques() {
 
 }
 
-dessineBriques();
+ // dessineBriques();
+
+ function dessine() {
+
+        if(fin === false) {
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                dessineBriques();
+                dessineBalle();
+                dessineBarre();
+                collisionDetection();
+
+                if(x + vitesseX > canvas.width - rayonBalle || x + vitesseX < rayonBalle) {
+                    vitesseX = -vitesseX;
+                }
+
+                if(y + vitesseY < rayonBalle) {
+                    vitesseY = -vitesseY
+                }
+
+                if(y + vitesseY > canvas.height - rayonBalle) {
+
+                    if(x > barreX && x < barreX + barreWidth) {
+
+                        vitesseX = vitesseX + 0.1;
+                        vitesseY = vitesseY + 0.1;
+                        vitesseY = -vitesseY;
+
+                    } 
+                    else {
+                        fin = true;
+                        affichageScore.innerHTML = `Perdu ! <br> Clique sur le casse-briques, pour recommencer.`
+
+                } 
+               
+                }
+
+
+                x += vitesseX;
+                y += vitesseY;
+                requestAnimationFrame(dessine);
+        }
+        
+        
+ }
+
+dessine();
+
+
+// collision des barres 
+
+function collisionDetection() {
+
+        for(let i = 0; i < nbRow; i++){
+            for(let j = 0; j < nbCol; j++){
+
+                    let b = briques[i][j];
+                    if(b.status === 1) {
+                        if(x > b.x && x < b.x + largeurBrique && y > b.y && y < b.y + hauteurBrique) {
+                            vitesseY = -vitesseY
+                            b.status = 0;
+    
+                            score++
+                            affichageScore.innerHTML = `Score : ${score}`;
+    
+                            if(score === nbCol * nbRow) {
+    
+                                    affichageScore.innerHTML = `Bravo ! <br> Clique sur le casse-briques, pour recommencer.`
+                                    fin = true;
+                            }
+                    } 
+                 }
+            }
+        }
+
+
+}
+
+
+
+
+
+// Mouvement de la barre 
+
+document.addEventListener('mousemove', mouvementSouris);
+
+    function mouvementSouris(e) {
+
+        let posXBarreCanvas = e.clientX - canvas.offsetLeft;
+
+        if(posXBarreCanvas > 35 && posXBarreCanvas < canvas.width - 35) {
+            barreX = posXBarreCanvas - barreWidth/2;
+        }
+
+    }
+
+
+
+
+
+
+// Recommencer une partie
+canvas.addEventListener('click', () => {
+
+    if(fin === true) {
+        fin = false;
+        document.location.reload();
+    }
+
+})
